ed7bf027a3fee3c5f578ae845fece5482697133b
KyleScharnhorst <scharkr1@uw.edu>
2017-09-01 20:51:11 -0700

    Change: first function app to take user input.
    doubles the integer provided by the user.

--- firstFunction/firstFunction.cpp
+++ firstFunction/firstFunction.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
+int getIntFromUser() {
+    int input;
+    std::cout << "Enter an integer: ";
+    std::cin >> input;
+    return input;
+}
+
 int doubleNumber(int x) {
     return 2 * x;
 }
 
 int main() {
-    std::cout << doubleNumber(6) << std::endl;
+    std::cout << doubleNumber(getIntFromUser()) << std::endl;
 
     return 0;
 }
