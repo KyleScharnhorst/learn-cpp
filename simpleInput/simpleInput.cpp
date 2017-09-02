a18a751ffb479a9302df821f72f08f942a6e891d
KyleScharnhorst <scharkr1@uw.edu>
2017-09-01 20:21:00 -0700

    Add: simple input app.
    gets user input.

---
+++ simpleInput/simpleInput.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int input = 0;
+    std::cout << "Provide an integer: ";
+    std::cin >> input;
+    std::cout << "The integer provided is: " << input << std::endl;
+    return 0;
+}
